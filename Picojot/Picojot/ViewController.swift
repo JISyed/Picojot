3390f26789783850de9ea7a1f04dcfdd0070c183
Jibran Ibrahim Syed <jisyed14@gmail.com>
2018-04-11 20:45:21 -0500

    Creating Xcode project

---
+++ Picojot/Picojot/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Picojot
+//
+//  Created by Jibran Syed on 4/11/18.
+//  Copyright © 2018 Jishenaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        
+    }
+    
+    
+}
+
