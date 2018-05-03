03b7bab666087836617a7ba99b29b7a3daaf296c
Jibran Ibrahim Syed <jisyed14@gmail.com>
2018-05-03 07:27:51 -0500

    Adding schema name access that's type safe

---
+++ Picojot/Picojot/Model/CloudKitSchemas.swift
@@ -0,0 +1,27 @@
+//
+//  CloudKitSchemas.swift
+//  Picojot
+//
+//  Created by Jibran Syed on 4/24/18.
+//  Copyright © 2018 Jishenaz. All rights reserved.
+//
+
+import Foundation
+
+
+enum JotSchema
+{
+    static let Title = "jotTitle"
+    static let ColorTagId = "jotTagId"
+    static let BodyText = "jotBody"
+}
+
+
+enum ColorTagSchema
+{
+    static let Name = "tagName"
+    static let ColorRed = "tagRedValue"
+    static let ColorGreen = "tagGreenValue"
+    static let ColorBlue = "tagBlueValue"
+}
+
